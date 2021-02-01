6eb31de7bc085d3bef8336521e7389d2c706f8a7
zzz <845538195@qq.com>
2021-02-01 11:51:39 +0800

    calculate_attach_point need to solve.

---
+++ main.py
@@ -0,0 +1,14 @@
+import math
+
+
+def calculate_attach_point(x: float, y: float, angle: float, width: int, height: int) -> tuple[float, float]:
+    """
+    计算图像旋转之后，附着点新的坐标（相对图像左上角）
+    :param x: 图像水平时，附着点的x坐标
+    :param y: 图像水平时，附着点的x坐标
+    :param angle: 图像旋转角
+    :param width: 图像原始宽度
+    :param height: 图像原始高度
+    :return: 新的附着点坐标
+    """
+    pass
